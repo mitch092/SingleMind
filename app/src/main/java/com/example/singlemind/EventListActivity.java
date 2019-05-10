08e3a0b258fa3dce6f0b0556d76b37e0940232a6
BlazinAmazin <kory.heinichen@gmail.com>
2019-05-09 22:08:29 -0700

    Added Modify Delete Dialog Box to event list click

--- app/src/main/java/com/example/singlemind/EventListActivity.java
+++ app/src/main/java/com/example/singlemind/EventListActivity.java
@@ -1,10 +1,12 @@
 package com.example.singlemind;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -16,13 +18,12 @@ import java.util.ArrayList;
 public class EventListActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
 
     MyRecyclerViewAdapter adapter;
+    ArrayList<Event> events = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
-
-        ArrayList<Event> events = new ArrayList<>();
 
 
 
@@ -45,6 +46,46 @@ public class EventListActivity extends AppCompatActivity implements MyRecyclerVi
     @Override
     public void onItemClick(View view, int position) {
         Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+
+        Event selected = new Event();
+        selected = events.get(position);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Event Options");
+        builder.setMessage("Modify, Delete, or Cancel");
+
+        builder.setPositiveButton("Modify",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //modify or update event OR delete current event and call add event
+
+                        dialog.cancel();
+                    }
+                }
+        );
+
+        builder.setNeutralButton("Delete",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //delete event
+
+                        dialog.cancel();
+                    }
+                }
+        );
+
+        builder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                }
+                );
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     public void onAcct(View view){
