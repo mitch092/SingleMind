b95ca9405d9c6fa87e126dc917633ba38456ec91
mitch092 <mitch092@cougars.csusm.edu>
2019-05-11 15:06:48 -0700

    Changed the event access object to be synchronous.

--- app/src/main/java/com/example/singlemind/backend/Event/AccessObjects/EventAccess.java
+++ app/src/main/java/com/example/singlemind/backend/Event/AccessObjects/EventAccess.java
@@ -1,8 +1,13 @@
 package com.example.singlemind.backend.Event.AccessObjects;
 
+import android.util.Log;
+
 import com.example.singlemind.backend.Event.TransferObjects.Event;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -26,18 +31,53 @@ public class EventAccess {
 
     }
 
-    public Call<Void> addEvent(Event event){
-        return service.addEvent(event);
+    public Boolean addEvent(Event event){
+        try {
+            return service.addEvent(event).execute().isSuccessful();
+        } catch(IOException e){
+            Log.d("http_IOException", "error adding events");
+            return false;
+        }
     }
 
-    public Call<Void> deleteEvent(int event_id){
-        return service.deleteEvent(event_id);
+    public Boolean deleteEvent(int event_id){
+        try {
+            return service.deleteEvent(event_id).execute().isSuccessful();
+        } catch(IOException e){
+            Log.d("http_IOException", "error deleting events");
+            return false;
+        }
     }
 
-    public Call<Void> updateEvent(Event event){ return service.updateEvent(event, event.getEventID());}
+    public Boolean updateEvent(Event event){
+        try {
+            return service.updateEvent(event, event.getEventID()).execute().isSuccessful();
+        } catch(IOException e){
+            Log.d("http_IOException", "error updating events");
+            return false;
+        }
+    }
 
-    public Call<List<Event>> getEventByEventID(int event_id){ return service.getEventByEventID(event_id);}
-    public Call<List<Event>> getEventsByUserID(int user_id){return service.getEventByUserID(user_id);}
+    public Optional<Event> getEventByEventID(int event_id){
+        try {
+            return Optional.of(service.getEventByEventID(event_id).execute().body().get(0));
+        } catch(IOException e){
+            Log.d("http_IOException", "error getting events by event id");
+            return Optional.empty();
+        } catch(NullPointerException e){
+            Log.d("http_nullptr_exception", "error getting events by event id");
+            return Optional.empty();
+        }
+    }
+    public List<Event> getEventsByUserID(int user_id){
+        try {
+            return service.getEventByUserID(user_id).execute().body();
+        } catch(IOException e){
+            Log.d("http_IOException", "error getting events by user id");
+            return Collections.emptyList();
+        }
+    }
+
 
 
     private OkHttpClient getOkHttpClient(){
